2e2d4fd8e0c648953877d1aa4c7eda2bb3ca21ec
Magdy371 <magdyelshrief37@gmail.com>
2025-09-28 12:04:04 +0300

    refactoring rdis_2

---
+++ src/redis/service.redis.ts
@@ -0,0 +1,52 @@
+import { Injectable, OnModuleDestroy, OnModuleInit, Inject } from '@nestjs/common';
+import { Redis } from 'ioredis';
+import { MongoService } from 'src/DB/mongo.service';
+import { Logger } from '@nestjs/common';
+
+@Injectable()
+export class RedisService implements OnModuleInit, OnModuleDestroy {
+    private readonly logger = new Logger(RedisService.name);
+    private redisClient: Redis;
+    private readonly LOGS_QUEUE = 'logs-queue';
+
+    constructor(private service: MongoService){
+        this.redisClient = new Redis({
+            host: process.env.REDIS_HOST || 'localhost',
+            port: Number(process.env.REDIS_PORT) || 6379,
+        });
+    }
+
+    async onModuleInit() {
+        await this.processLogQueue();
+        this.logger.log('Redis service initialized with log consumer');
+    }
+
+    async onModuleDestroy() {
+        await this.redisClient.quit();
+    }
+
+    async publishLog(logData: any): Promise<void>{
+        try {
+            await this.redisClient.lpush(this.LOGS_QUEUE, JSON.stringify(logData));
+        } catch (e) {
+            this.logger.error(`Failed to publish log to redis ${e.message}`);
+        }
+    }
+
+    private async processLogQueue(): Promise<void> {
+        try {
+            while (true) {
+                const result = await this.redisClient.brpop(this.LOGS_QUEUE, 0);
+                
+                if (result) {
+                    // result[0] = key name, result[1] = actual value
+                    const logData = JSON.parse(result[1]);
+                    await this.service.createLog(logData);
+                    this.logger.debug('Log saved to MongoDB via Redis Queue');
+                }
+            }
+        } catch (error) {
+            this.logger.error('Failed to process log queue', error);
+        }
+    }
+}
